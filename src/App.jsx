13fbe163c9b27aabce85505aed423b8eb884c762
SyedBasit <basityar6@gmail.com>
2024-05-16 14:53:02 +0530

    Todos component design WIP

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,13 @@ function App() {
           <Form />
         </div>
 
-        <div>
+        <div className="bg-violet-300 py-5 px-8 flex flex-wrap justify-center">
+          <Todos />
+          <Todos />
+          <Todos />
+          <Todos />
+          <Todos />
+          <Todos />
           <Todos />
         </div>
       </div>
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,6 +3,16 @@
 export default function Todos() {
     return (
         <>
+            <div className="flex flex-col w-64 border-2 border-red-700 px-3 py-3 bg-yellow-200 my-2 mx-3">
+                <div className="border border-x-0 border-t-0 pb-2 border-black">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit nam tempora unde consequatur, necessitatibus obcaecati aspernatur nihil odit optio possimus modi? Architecto eius ea facilis.</p>
+                </div>
+                <div className="flex justify-around items-center pt-2">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                    <input type="checkbox" />
+                </div>
+            </div>
         </>
     );
 }
