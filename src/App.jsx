f4c9bfcb4bfc00fcc16bd4a20c85e613a82fe09d
SyedBasit <basityar6@gmail.com>
2024-05-16 20:07:19 +0530

    modify a form to make it  sticky

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ function App() {
 
     <>
       <div className="border-2 border-red-600">
-        <div className="bg-yellow-300 py-5 px-8">
+        <div className="bg-yellow-200 py-5 px-8 sticky left-0 top-0">
 
           <Form />
         </div>
 
         <div className="bg-violet-300 py-5 md:px-8 flex flex-wrap justify-center">
+          <Todos />
+          <Todos />
+          <Todos />
+          <Todos />
           <Todos />
           <Todos />
           <Todos />
