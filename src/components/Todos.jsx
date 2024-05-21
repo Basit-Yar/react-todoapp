157f5f100bf4af29150fed7b2f29e22cc25fd4be
SyedBasit <basityar6@gmail.com>
2024-05-21 18:19:04 +0530

    toggle and delete todo funcitonality added

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,17 +1,40 @@
-
+import { useContext, useState } from "react";
+import TodoContext from "../context/TodoContext";
 
 export default function Todos({ todo }) {
-    
+
+    const { updateTodo, deleteTodo, toggleCompleteTodo } = useContext(TodoContext)
+
+    // const { isDone, setIsDone } = useState(false)
+
+    const trashTodo = () => {
+        deleteTodo(todo.id);
+    }
+
+    const toggleCompleteTodoInfo = () => {
+        toggleCompleteTodo(todo.id);
+    }
+
     return (
         <>
             <div className="flex flex-col md:w-64 border-2 border-red-700 px-3 py-3 bg-yellow-200 my-3 mx-3">
-                <div className="border border-x-0 border-t-0 pb-2 border-black">
+                <div className={`border border-x-0 border-t-0 pb-2 border-black ${todo.isCompleted ? "line-through text-gray-400" : ""}`}>
                     <p>{todo.task}</p>
                 </div>
                 <div className="flex justify-around items-center pt-2">
                     <button>Edit</button>
-                    <button>Delete</button>
-                    <input type="checkbox" />
+                    <button
+                        className=""
+                        onClick={trashTodo}
+                    >
+                        Delete
+                    </button>
+                    <input
+                        style={{ accentColor: "red", height: "16px", width: "16px" }}
+                        type="checkbox"
+                        checked={todo.isCompleted}
+                        onChange={toggleCompleteTodoInfo}
+                    />
                 </div>
             </div>
         </>
